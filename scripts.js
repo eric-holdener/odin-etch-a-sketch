6e25fa61acb302ac1727ac2a4c6343b15572e9c5
EHoldener <eholdener12@gmail.com>
2022-03-07 09:57:49 -0800

    got button kind of functioning, at the moment it fires when the page is loaded then wont again. Need to fix it to be clickable always

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', function() {
-    createGrid();
+    createGrid(16);
 });
 
-function createGrid() {
+function createGrid(newColumnLength) {
     let gridContainer = document.getElementById('grid-container');
+    gridContainer.style.gridtemplatecolumns = newColumnLength;
 
     if (gridContainer.childNodes.length > 0) {
         while (gridContainer.firstChild) {
@@ -15,19 +16,18 @@ function createGrid() {
     let y = Math.floor(window.innerHeight / 50);
     
     console.log('creating boxes');
-    for (let i = 0; i < x*y && i < 256; i++) {
+    for (let i = 0; i < x*y && i < newColumnLength**2; i++) {
         let box = document.createElement('div');
         box.className = 'box';
         box.addEventListener('mouseover', (event) => {
-            console.log(event.target);
             event.target.className = 'box-transition';
-            console.log(event.target.className);
         })
         gridContainer.appendChild(box);
     }
-    console.log('boxes created');
 }
 
-window.addEventListener('resize', function() {
-    createGrid();
-});
+function clearGame() {
+    let gameSize = prompt('What size of board do you want?', 16);
+    gameSize = parseInt(gameSize);
+    createGrid(gameSize);
+}
