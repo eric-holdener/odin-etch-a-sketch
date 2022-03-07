4dcbd5c54fe21405abf6195195bce3523415512d
EHoldener <eholdener12@gmail.com>
2022-03-06 20:43:45 -0800

    created initial create grid for etch a sketch, event listeners for load and resize run create grid, which will wipe the grid and reset it based on the size of the window

---
+++ scripts.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+    createGrid();
+});
+
+function createGrid() {
+    let gridContainer = document.getElementById('grid-container');
+
+    if (gridContainer.childNodes.length > 0) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+    }
+
+    let x = Math.floor(window.innerWidth / 50);
+    let y = Math.floor(window.innerHeight / 50);
+    
+    for (let i = 0; i < x*y && i < 256; i++) {
+        let box = document.createElement('div');
+        box.className = 'box';
+        gridContainer.appendChild(box);
+    }
+}
+
+window.addEventListener('resize', function() {
+    createGrid();
+
+});
